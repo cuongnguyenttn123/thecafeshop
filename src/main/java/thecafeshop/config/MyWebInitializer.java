11b4c459acfced0d8701c32fa554c35135965a8a
cuong.nguyenvan <cuong.codecomplete@gmail.com>
2019-11-12 16:14:23 +0700

    the first project the cafe shop

---
+++ src/main/java/thecafeshop/config/MyWebInitializer.java
@@ -0,0 +1,21 @@
+package thecafeshop.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+public class MyWebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{
+
+        };
+    }
+
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{
+                SpringWebConfig.class
+        };
+    }
+
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: cuong
+  Date: 12/11/2019
+  Time: 04:00 CH
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h3>Chao mung toi voi do an tot nghiep cua Cuong Nguyen</h3>
+</body>
+</html>
